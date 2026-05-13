d9a1c9b2209a772d7deb38b1fe466c0f8d580bd7
tanisha <tanishasingh2401@gmail.com>
2026-05-14 00:04:05 +0530

    CLUB CONNECT PROJECT

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /**
  * Club Connect - Core Logic
  */
-//javacript code is implementeds
+//javacript code is implementeds work
 
 document.addEventListener("DOMContentLoaded", () => {
     // 1. Dynamic Greeting System
